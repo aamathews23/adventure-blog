c4db73238f7ef265164f6f2f39d119fe4b71df8f
Aaron Mathews <aamathews23@gmail.com>
2022-06-03 18:05:45 -0700

    added markdown page config file for home page rel: #17

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Page from '../components/layout/Page';
+import { attributes, react as HomeContent } from '../content/pages/home.md';
 
 const Home = () => (
-  <Page
-    title="Aaron's Adventures"
-    description="A blog about my adventures around the Pacific Northwest!"
-  >
-    <h1>Hello</h1>
-  </Page>
+  <>
+    <HomeContent />
+    <Page
+      title={attributes.title}
+      description={attributes.description}
+    >
+      <h1>Hello</h1>
+    </Page>
+  </>
 );
 
 export default Home;
