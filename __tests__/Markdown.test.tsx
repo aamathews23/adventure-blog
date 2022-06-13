8c6c58ea276df74ec3db6b64d85d8af8ee5821b4
Aaron Mathews <aamathews23@gmail.com>
2022-06-12 18:35:40 -0700

    created unit tests for the markdown component

---
+++ __tests__/Markdown.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import Markdown from '../components/posts/Markdown';
+import '@testing-library/jest-dom';
+
+const text = 'Hello world';
+const content = `# ${text}`;
+
+describe('Markdown', () => {
+  it('mounts', () => {
+    const { getByTestId } = render(<Markdown content={content} />);
+    const markdown = getByTestId('markdown');
+    expect(markdown).toBeInTheDocument();
+  });
+  it('renders the content', () => {
+    const { getByText } = render(<Markdown content={content} />);
+    const el = getByText(text);
+    expect(el).toBeInTheDocument();
+    expect(el.nodeName).toEqual('H1');
+  });
+});
