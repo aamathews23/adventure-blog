6f8433fd714de872bb9031f9ca1d8cfaf6e4adc9
Aaron Mathews <aamathews23@gmail.com>
2022-06-18 19:26:21 -0700

    created a not found component rel: #29

---
+++ __tests__/NotFound.test.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import NotFound from '../components/global/NotFound';
+import '@testing-library/jest-dom';
+
+const text = 'Hello world';
+
+describe('NotFound', () => {
+  it('mounts', () => {
+    const { getByTestId } = render(<NotFound text={text} />);
+    const notFound = getByTestId('not-found');
+    expect(notFound).toBeInTheDocument();
+  });
+  it('has the text', () => {
+    const { getByText } = render(<NotFound text={text} />);
+    expect(getByText(text)).toBeInTheDocument();
+  });
+});
