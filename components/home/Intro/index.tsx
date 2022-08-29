0553d1b2b1eefcee2329c87865c1c88041150b78
Aaron Mathews <aamathews23@gmail.com>
2022-08-28 18:15:27 -0700

    refactored the intro component rel: #103

--- components/home/Intro/index.tsx
+++ components/home/Intro/index.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
 import Text from '../../global/Text';
 import Button from '../../global/Button';
 import Markdown from '../../global/Markdown';
 import IntroType from '../../../types/home/Intro';
 import style from './style';
 
-const Intro = ({ title, main, cta }: IntroType) => {
+const loader = ({ src }: any) => {
+  return src;
+};
+
+const Intro = ({ title, main, cta, image }: IntroType) => {
   return (
     <section
       css={style.section}
@@ -27,6 +32,21 @@ const Intro = ({ title, main, cta }: IntroType) => {
           label={cta.label}
           tag={cta.tag}
           anchor={cta.anchor}
+        />
+      </div>
+      <div
+        css={style.img}
+        data-testid="intro-img"
+      >
+        <Image
+          src={image.src}
+          alt={image.alt}
+          width="500"
+          height="500"
+          objectFit="cover"
+          loader={loader}
+          unoptimized
+          priority
         />
       </div>
     </section>
