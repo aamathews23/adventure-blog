5751ff084fea46b0753c06e777cf565a245792b3
Aaron Mathews <aamathews23@gmail.com>
2022-09-13 21:28:13 -0700

    added code styling to markdown comp rel: #135

--- components/global/Markdown/index.tsx
+++ components/global/Markdown/index.tsx
@@ -1,6 +1,8 @@
 import { Remarkable } from 'remarkable';
+import hljs from 'highlight.js';
 import MarkdownType from '../../../types/global/Markdown';
 import style from './style';
+import 'highlight.js/styles/github-dark.css';
 
 /**
  * Creates a markdown component to be used throughout the app
@@ -9,7 +11,21 @@ import style from './style';
  * @returns a markdown component
  */
 const Markdown = ({ content }: MarkdownType) => {
-  const md = new Remarkable();
+  const md = new Remarkable({
+    highlight: function (str: string, lang: string) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(str, { language: lang }).value;
+        } catch (err) {}
+      }
+  
+      try {
+        return hljs.highlightAuto(str).value;
+      } catch (err) {}
+  
+      return '';
+    }
+  });
   return (
     <section
       css={style}
